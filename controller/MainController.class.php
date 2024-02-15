cca2318371139e239d38de922ee19e27ef563880
Polm04 <pol.mr.2003@gmail.com>
2024-02-15 20:13:12 +0100

    main controller

--- controller/MainController.class.php
+++ controller/MainController.class.php
@@ -10,34 +10,34 @@ class MainController
     {
 
         include("view/menu/MainMenu.html");
-        
-        // recuperem l' opció d'un menú
-        if (isset($_GET["menu"])) {
 
-            $request = $_GET["menu"];
-        } else {
+        // // recuperem l' opció d'un menú
+        // if (isset($_GET["menu"])) {
 
-            $request = NULL;
-        }
+        //     $request = $_GET["menu"];
+        // } else {
 
-        //mirem de quin menú venim
-        switch ($request) {
-                // si hi haguessin molts controladors, faríem un case per cadascun d'ells. Aquí 
-                // per defecte fiquem l'únic controlador que hi ha CategoryController
-                // en el cas que hi haguessin molts:
-            case "history": // URL: [...]/index.php?menu=pet
-                $historyController = new HomeController();
-                $historyController->processRequest();
-                break;
-                //ficaríem un case per cada controlador
+        //     $request = NULL;
+        // }
 
-                //en el cas que volguessim carregar alguna vista per defecte fora de la que ens vindrà dels controladors
-                //per a nosaltres, la vista primera és la que ens ofereix el menú de categories
+        // //mirem de quin menú venim
+        // switch ($request) {
+        //         // si hi haguessin molts controladors, faríem un case per cadascun d'ells. Aquí 
+        //         // per defecte fiquem l'únic controlador que hi ha CategoryController
+        //         // en el cas que hi haguessin molts:
+        //     case "history": // URL: [...]/index.php?menu=pet
+        //         $historyController = new HomeController();
+        //         $historyController->processRequest();
+        //         break;
+        //         //ficaríem un case per cada controlador
 
-            default:
-                $historyController = new HomeController();
-                $historyController->processRequest();
-                break;
-        }
+        //         //en el cas que volguessim carregar alguna vista per defecte fora de la que ens vindrà dels controladors
+        //         //per a nosaltres, la vista primera és la que ens ofereix el menú de categories
+
+        //     default:
+        //         $historyController = new HomeController();
+        //         $historyController->processRequest();
+        //         break;
+        // }
     }
 }
