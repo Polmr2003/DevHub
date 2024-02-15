d317f0d0f8d6f418bc02f7815d21434225389e0a
Polm04 <pol.mr.2003@gmail.com>
2024-02-15 20:18:32 +0100

    a

--- view/form/OwnerForm/OwnerDeleteMessageShow.php
+++
@@ -1,5 +0,0 @@
-<div id="content">
-    <h1>
-        Deleted Successfully
-    </h1>
-</div>
--- view/js/general-fn.js
+++
@@ -1,9 +0,0 @@
-function form_delete(form_id) {
-    if (confirm("Delete?")) {
-      document.getElementById(form_id).submit();
-    }
-}
-
-function form_reset(form_id) {
-    document.getElementById(form_id).submit();
-}
